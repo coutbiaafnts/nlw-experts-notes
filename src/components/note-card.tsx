17a4122ded800ee3067ad14a6f2a287489e49933
coutbiaafnts <bia.fontes653@gmail.com>
2024-02-07 19:10:23 -0300

    feat: set components with props in the app

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,15 +1,18 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({note}: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-6 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+    <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
       <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ratione
-        modi corrupti nihil iste voluptate nobis dicta, reprehenderit
-        praesentium commodi illo. Quibusdam cum quae velit unde ea excepturi
-        illum beatae. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Quam ratione modi corrupti nihil iste voluptate nobis dicta,
-        reprehenderit praesentium commodi illo. Quibusdam cum quae velit unde ea
-        excepturi illum beatae.
+        {note.content}
       </p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/50 to-black/0 pointer-events-none" />
