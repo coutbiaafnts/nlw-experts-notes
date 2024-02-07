87a49202b227c2fd2ab4f68f8387f8ffbfc29b89
coutbiaafnts <bia.fontes653@gmail.com>
2024-02-07 20:32:49 -0300

    feat: add button close and delete to modal view note

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,8 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { formatDistanceToNow } from "date-fns";
-import { ptBR } from "date-fns/locale";
+import * as Dialog from "@radix-ui/react-dialog"
+import { formatDistanceToNow } from "date-fns"
+import { ptBR } from "date-fns/locale"
+import { X } from 'lucide-react'
+
 
 interface NoteCardProps {
   note: {
@@ -23,9 +25,12 @@ export function NoteCard({note}: NoteCardProps) {
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/50 to-black/0 pointer-events-none" />
       </Dialog.Trigger>
 
-      <Dialog.DialogPortal>
-        <Dialog.DialogOverlay className="inset-0 fixed bg-black/50"/>
-        <Dialog.DialogContent className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
+        <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+          <X className="size-5"/>
+        </Dialog.Close>
           <div className="flex flex-1 flex-col ga-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
@@ -35,8 +40,15 @@ export function NoteCard({note}: NoteCardProps) {
               {note.content}
             </p>
           </div>
-        </Dialog.DialogContent>
-      </Dialog.DialogPortal>
+
+          <button 
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
     </Dialog.Root>
   );
 }
